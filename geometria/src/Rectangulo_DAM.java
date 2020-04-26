8219f9d29756b03b6192126131b4c81a9eed73ad
danaska <danaska.alonso@gmail.com>
2020-04-26 09:13:43 +0200

    versión comentada

--- geometria/src/Rectangulo_DAM.java
+++ geometria/src/Rectangulo_DAM.java
@@ -1,18 +1,29 @@
+/**
+ * @author $danaska alonso
+ *
+ * $version 1.2
+ */
 
 public class Rectangulo_DAM extends FiguraGeometrica_DAM {
 	private double l1;
 	private double l2;
+	
+	//Dibuja el rectángulo
 	
 	public Rectangulo_DAM(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
+	
+	//Calcula el área
 
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
+	
+	//Calcula el perímetro
 	
 	@Override
 	public double perimetro() {
